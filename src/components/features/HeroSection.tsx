4c2e77867e4936b4f6a8a801b0dc880513189bdb
Kavin Sashantha <23.kavinsk@gmail.com>
2025-08-20 11:20:41 +0530

    finalised 6

--- src/components/features/HeroSection.tsx
+++ src/components/features/HeroSection.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ChevronLeft, ChevronRight, ArrowRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import hambantotaImg from "../../assets/images/hambantota.jpg";
 import kataragamaImg from "../../assets/images/kataragama.jpg";
 import tangalleImg from "../../assets/images/tangalle.jpg";
